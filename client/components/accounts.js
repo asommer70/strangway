bb9c99aad83c49c56cf2112643fc3b60c1dbe465
Adam Sommer <adam@thehoick.com>
2017-09-28 06:41:24 -0400

    Importing changes...

--- client/components/accounts.js
+++ client/components/accounts.js
@@ -4,12 +4,18 @@ import { Template } from 'meteor/templating';
 import { Blaze } from 'meteor/blaze';
 
 class Accounts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {error: ''}
+  }
 
   login(e) {
     e.preventDefault();
     Meteor.loginWithPassword(this.refs.email.value, this.refs.password.value, (error) => {
       console.log('error:', error);
-      if (!error) {
+      if (error) {
+        this.setState({error: error.message});
+      } else {
         window.location.href = '/';
       }
     });
@@ -33,6 +39,9 @@ class Accounts extends Component {
               </label>
             <input type="password" id="password" ref="password" name="at-field-password" placeholder="Password" aria-label="Password " />
           </div>
+        </div>
+        <div>
+          {this.state.error}
         </div>
         <button type="submit" className="button small">Sign In</button>
       </form>
