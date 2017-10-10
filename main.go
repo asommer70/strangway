b845590d806906952172da57f613177040aa7bb5
Adam Sommer <adam@thehoick.com>
2017-10-10 14:42:08 -0400

    Changed web server to use a couple of static functions for Index and Query.  Query takes GraphQL query and resturns JSON.

--- main.go
+++ main.go
@@ -3,23 +3,45 @@ package main
 import (
 	"log"
 	"github.com/julienschmidt/httprouter"
-	"strangway/controllers"
-	"strangway/db"
 	"net/http"
 	"os"
+	"encoding/json"
+	"strangway/schema"
+	"strangway/db"
+	"strangway/models"
 )
+
+func init() {
+	ldb := db.Connect()
+	models.CreateTables(ldb)
+}
 
 func main() {
 	log.Println("Executing Strangway!...")
 	//config := db.GetConfig();
-	db := db.Connect()
-	nc := controllers.NewNoteController(db)
+	//db := db.Connect()
+	//nc := controllers.NewNoteController(db)
 
 	router := httprouter.New()
-	router.GET("/", nc.Find)
-	router.ServeFiles("/assets/*filepath", http.Dir("./assets/"))
+	router.GET("/", Index)
+	router.GET("/:query", Query)
+	//router.ServeFiles("/assets/*filepath", http.Dir("./assets/"))
 
 	// TODO:as adjust port to use config file.
 	log.Println("Listening on port: ", os.Getenv("PORT"))
 	http.ListenAndServe("localhost:" + os.Getenv("PORT"), router)
+}
+
+func Index(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	res.Header().Set("Content-Type", "application/json")
+	intro := struct{Message string `json:"message"`}{
+		Message: "Welcome to the GraphQL version of Strangway.",
+	}
+	json.NewEncoder(res).Encode(intro)
+}
+
+func Query(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	res.Header().Set("Content-Type", "application/json")
+	result := schema.ExecuteQuery(params.ByName("query"), schema.Schema)
+	json.NewEncoder(res).Encode(result)
 }
--- models/migrations.go
+++ models/migrations.go
@@ -2,16 +2,19 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
+	"log"
 )
 
 func CreateTables(db *gorm.DB) {
 	nt := db.HasTable(&Note{})
 	if !nt {
+		log.Println("CreateTables notes...")
 		db.AutoMigrate(&Note{})
 	}
 
 	ft := db.HasTable(&Folder{})
 	if !ft {
+		log.Println("CreateTables folders...")
 		db.AutoMigrate(&Folder{})
 	}
 }
