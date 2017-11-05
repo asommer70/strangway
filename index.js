271c4f18a3da5293933c8ebccce9dbce3ba00b82
Adam Sommer <adam@thehoick.com>
2017-11-05 14:39:21 -0500

    Enabled CORS.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const cors = require('cors');
 require('dotenv').config();
 const schema = require('./schema');
 
 const app = express();
 
+app.use(cors());
 app.use('/graphql', expressGraphQL({
   schema,
   graphiql: true
