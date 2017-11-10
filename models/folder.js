838a6155a77b39d646dc5ed74f75c2a7fb3b0206
Adam Sommer <adam@thehoick.com>
2017-11-10 14:27:14 -0500

    Using formatted date for Notes.updatedAt field.

--- models/folder.js
+++ models/folder.js
@@ -34,7 +34,7 @@ var Folder = function(attrs) {
     this.notes = () => {
       const db = DB.con();
       const query = {
-        text: `select * from notes where folderId = $1 order by notes.updatedat desc;`,
+        text: `select *, to_char(updatedat, 'MM-DD-YYYY HH:MI:SS') as updatedat from notes where folderId = $1 order by notes.updatedat desc;`,
         values: [this.id]
       }
 
