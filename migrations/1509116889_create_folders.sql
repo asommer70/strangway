f998313f02c3ba063d144e10e56d575006008fdb
Adam Sommer <asommer70@gmail.com>
2018-12-31 02:55:04 -0500

    Removing graphql files from master.

--- migrations/1509116889_create_folders.sql
+++
@@ -1,8 +0,0 @@
--- Migration created: Fri Oct 27 11:08:09 EDT 2017
-
-create table if not exists folders (
-  id    serial primary key,
-  name   varchar(40),
-  createdAt timestamp default now(),
-  updatedAt timestamp default now()
-);
--- migrations/1509117956_folders_have_many_notes.sql
+++
@@ -1,9 +0,0 @@
--- Migration created: Fri Oct 27 11:25:56 EDT 2017
-
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'belongstofolder') THEN
-    alter table notes add constraint belongstofolder foreign key (folderId) references folders (id);
-  END IF;
-END;
-$$;
